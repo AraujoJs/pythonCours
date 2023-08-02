c0021d1b03d2f87c5a38120a76add1324b8b24d8
ARAUJO 'José' Arthur <araujoo.java@gmail.com>
2023-08-02 02:08:24 +0200

    Update nim.py
    removendo bugs do joguinho de matematica chamado "NIM"! ;)

--- Games/nim.py
+++ Games/nim.py
@@ -1,193 +1,146 @@
-"""
-Trabalho final - jogo NIM
-
-1 - Menu com opções
-
-2 - Gerar tabuleiro aleatorio
-
-3 - Solicitar a jogada ao jogador
-
-4 - Validar jogada
-
-5 - Se valido, mostrar o tabuleiro atualizado + solicitar jogada ao jogador 2
- se não: Mostrar mensagem "opção invalida" e solicitar novamente a jogada ao jogador 1
-
-"""
 import random
 
-# Gerar tabuleiro inicial
-tabuleiro = list()
 def menu():
-    global tabuleiro
+    # Função para mostrar o menu com as opções do jogo
     print("""
 0 - Sair
 1 - Humano vs Humano
 2 - Humano vs Computador
     """)
 
-    # Gerar tabuleiro com numeros aleatorios
-    tabuleiro = [random.randint(1, 30), random.randint(1, 30), random.randint(1, 30),
-                              random.randint(1, 30)]
+    option = -1  # Entra no loop e aguarda uma opção que seja válida do jogador, ou seja, 0,1 ou 2
+    while option not in (0, 1, 2):
+        # Recebe a opção do jogador até que seja uma opção válida (0, 1 ou 2)
+        option = int(input("Opção:"))
 
-
-
-    # Validar jogadas
-    opcao = -1
-    while 0 > opcao or opcao > 2:
-        opcao = int(input("Opção:"))
-# Iniciando jogo
-    if opcao == 1 or opcao == 2:
-        iniciarGame(opcao)
-# Saindo do jogo
-    elif opcao == 0:
+    if option == 1 or option == 2:
+        # Inicia o jogo com a opção escolhida pelo jogador
+        iniciarJogo(option)
+    elif option == 0:
+        # Encerra o jogo
         print("Obrigado por jogar o meu jogo!")
 
-# Verificar se a jogada é valida: verificando se os valores são menores que o do tabuleiro e não são valores negativos
-def verificarJogada(p1, p2, p3, p4):
-    jogadas = [p1, p2, p3, p4]
-    x = jogadas.count(0)
+def gerarTabuleiro():
+    # Função para gerar um tabuleiro com quatro pilhas com valores aleatórios entre 1 e 30
+    tabuleiro = []
+    for i in range(0, 4):
+        tabuleiro.append(random.randint(1, 30))
+    return tabuleiro
 
-    if 1 > x > 3:
-        return False
+def mostrarTabuleiro(tabuleiro):
+    # Função para exibir o estado atual do tabuleiro
+    print("Tabuleiro:", *tabuleiro)
 
-    elif  p1 < 0 or p1 > tabuleiro[0]:
+# len -> length
+def validarJogada(jogadas, tabuleiro):
+    # Função para verificar se as jogadas são válidas (comparando com o tabuleiro que tem 4 pilhas)
+    if len(jogadas) != 4:
         return False
+    # contar o numero de jogadas que o usuario digitou (numero de pilhas -> max 2, min 1)
+    total_jogadas = 0
+    for jogada in jogadas: # lê-se "para cada elemento dentro da lista jogadas, repetir...
+        if jogada > 0:
+            total_jogadas = total_jogadas + 1
+    # Verificar se o total de jogadas é = 0 ou superior a 2 (não se pode tirar de mais de 2 pilhas)
+    if total_jogadas == 0 or total_jogadas > 2:
+        return False
+    # TABULEIRO = [7, 29, 14, 2]
+    # JOGADA = [7, 20, 0, 0]
+    for i in range(0, len(jogadas)):
+        if jogadas[i] < 0 or jogadas[i] > tabuleiro[i]:
+            return False
+    return True
 
-    elif p2 < 0 or p2 > tabuleiro[1]:
-        return False
-    elif p3 < 0 or p3 > tabuleiro[2]:
-        return False
-    elif p4 < 0 or p4 > tabuleiro[3]:
-        return False
-    else:
-        return True
-
-def fazerJogada(p1, p2, p3, p4):
-    # Criar lista com as jogadas do parametro
-    jogadas = [p1, p2, p3, p4]
-
+def fazerJogada(jogadas, tabuleiro, vez_computador):
+    # Função para efetuar as jogadas no tabuleiro
     for i in range(0, 4):
         tabuleiro[i] -= jogadas[i]
 
-def iniciarGame(opcao):
-    vezJogador1 = True
-    fim = False
+    if vez_computador:
+        print("Jogada do computador:", jogadas[0], jogadas[1], jogadas[2], jogadas[3])
 
-    # Opção HUMANO vs HUMANO
-    if opcao == 1:
-        # Enquanto não acabar o jogo, faça o loop:
-        while not fim:
-            # Mostrar o tabuleiro
-            print("Tabuleiro:", tabuleiro[0], tabuleiro[1], tabuleiro[2], tabuleiro[3])
+def fimDoJogo(tabuleiro):
+    # Função para verificar se todas as pilhas do tabuleiro estão vazias
+    for jogo in tabuleiro:
+        if jogo != 0:
+            return False
+    return True
 
-            # Se for vez do jogador 1, faça isso:
-            if vezJogador1:
-                #Pedir a jogada ao jogador, e armazenar cada valor em uma variavel
-                p1, p2, p3, p4 = eval(input("Jogador 1?\nInsere a jogada\n"))
 
-                # Verificar se a jogada foi valida
-                jogadaValida = verificarJogada(p1, p2, p3, p4)
+# 20 30 0 0
 
-                # Se a jogada não for valida, faça isso:
-                if not jogadaValida:
-                    # Enquanto a jogada não for valida, faça isso:
-                    while not jogadaValida:
-                        print("Opção invalida!")
-                        # Pedir novamente a jogada
-                        p1, p2, p3, p4 = eval(input("Jogador 1?\nInsere a jogada\n"))
-                        # Verificar novamente se a jogada foi valida
-                        jogadaValida = verificarJogada(p1, p2, p3, p4)
+# jogadas = ["20", "30", "0", "0"]
+def jogadaHumano(jogador):
+    # Função para receber a jogada de um jogador humano
 
-                # Efetuar a jogada:
-                fazerJogada(p1, p2, p3, p4)
+    pergunta = "Jogador " + str(jogador) + "?\nInsira a jogada separada por espaços: "
+    jogadas_string = input(pergunta).split()
+    
+    # ["20", "10", "0", "0"]
 
-                # Se todos os valores do tabuleiro são 0, fim do jogo
-                if tabuleiro.count(0) == 4:
-                    # Definir a variavel FIM como True
-                    fim = True
+    jogadas_int = []
 
-                    if vezJogador1:
-                        print("O vencedor foi o jogador 1!")
-                    else:
-                        print("O vencedor foi o jogador 2!")
-                # Passar a vez para o jogador 2, Definir a vez do jogador 1 como False
-                vezJogador1 = False
+    # String -> Int (convertir)
+    for jogada in jogadas_string:
+        jogadas_int.append(int(jogada))
 
-            # opção HUMANO vs COMPUTADOR
+    return jogadas_int
+
+
+def jogadaComputador(tabuleiro):
+    # Função para gerar uma jogada aleatória para o computador
+    # jogadas = [random.randint(0, x) for x in tabuleiro]
+
+    jogadas = []
+
+    while not validarJogada(jogadas, tabuleiro):
+        for pilha in tabuleiro:
+            jogadas.append(random.randint(0, pilha))
+
+    return jogadas
+
+def iniciarJogo(opcao):
+    # Função para iniciar o jogo com a opção escolhida pelo jogador
+    tabuleiro = gerarTabuleiro()
+    vez_jogador1 = True
+    vez_computador = False
+
+    while not fimDoJogo(tabuleiro):
+        # Loop principal do jogo, continua enquanto o jogo não terminar
+
+        mostrarTabuleiro(tabuleiro)
+
+        if vez_jogador1:
+            # É a vez do jogador 1 (ou humano, dependendo da opção escolhida)
+            jogadas = jogadaHumano(1)
+        else:
+            # É a vez do jogador 2 (ou computador, dependendo da opção escolhida)
+            if opcao == 1:
+                jogadas = jogadaHumano(2)
             else:
+                jogadas = jogadaComputador(tabuleiro)
+                vez_computador = True
 
-                #Pedir a jogada ao jogador, e armazenar cada valor em uma variavel
-                p1, p2, p3, p4 = eval(input("Jogador 2?\nInsere a jogada\n"))
+        while not validarJogada(jogadas, tabuleiro):
+            # Continua pedindo jogadas até que o jogador faça uma jogada válida
+            print("Opção inválida!")
+            if vez_jogador1:
+                jogadas = jogadaHumano(1)
+            else:
+                jogadas = jogadaHumano(2) if opcao == 1 else jogadaComputador(tabuleiro)
 
-                # Verificar se a jogada foi valida
-                jogadaValida = verificarJogada(p1, p2, p3, p4)
+        fazerJogada(jogadas, tabuleiro, vez_computador)
+        vez_jogador1 = not vez_jogador1
+        vez_computador = False
 
-                if not jogadaValida:
-                    while not jogadaValida:
-                        print("Opção invalida!")
-                        p1, p2, p3, p4 = eval(input("Jogador 2?\nInsere a jogada\n"))
-                        jogadaValida = verificarJogada(p1, p2, p3, p4)
-
-                fazerJogada(p1, p2, p3, p4)
-                if tabuleiro.count(0) == 4:
-                    fim = True
-                    if vezJogador1:
-                        print("O vencedor foi o jogador 1!")
-                    else:
-                        print("O vencedor foi o jogador 2!")
-                vezJogador1 = True
-
+    # Fim do jogo
+    mostrarTabuleiro(tabuleiro)
+    if vez_jogador1:
+        print("O vencedor foi o jogador 2!" if opcao == 1 else "O vencedor foi o computador!")
     else:
-        while not fim:
-            print("Tabuleiro:", tabuleiro[0], tabuleiro[1], tabuleiro[2], tabuleiro[3])
-
-            if vezJogador1:
-                p1, p2, p3, p4 = eval(input("Jogador 1?\nInsere a jogada\n"))
-                jogadaValida = verificarJogada(p1, p2, p3, p4)
-                if not jogadaValida:
-                    while not jogadaValida:
-                        print("Opção invalida!")
-                        p1, p2, p3, p4 = eval(input("Jogador 1?\nInsere a jogada\n"))
-                        jogadaValida = verificarJogada(p1, p2, p3, p4)
-
-                fazerJogada(p1, p2, p3, p4)
-                if tabuleiro.count(0) == 4:
-                    fim = True
-                    if vezJogador1:
-                        print("O vencedor foi o jogador 1!")
-                    else:
-                        print("O vencedor foi o jogador 2!")
-
-                vezJogador1 = False
-
-            else:
-                jogadas = [0, 0, 0, 0]
-
-                while not 1 < jogadas.count(0) < 4:
-                    jogadas = [
-                        random.randint(0,tabuleiro[0]),
-                        random.randint(0,tabuleiro[1]),
-                        random.randint(0,tabuleiro[2]),
-                        random.randint(0,tabuleiro[3])
-                    ]
-
-                print("Computador: \n", jogadas[0], jogadas[1], jogadas[2], jogadas[3])
-
-                fazerJogada(jogadas[0], jogadas[1], jogadas[2], jogadas[3])
-
-                if tabuleiro.count(0) == 4:
-                    fim = True
-                    if vezJogador1:
-                        print("O vencedor foi o jogador 1!")
-                    else:
-                        print("O vencedor foi o computador!")
-                vezJogador1 = True
+        print("O vencedor foi o jogador 1!")
 
     menu()
 
-
+# Inicia o jogo chamando a função menu()
 menu()
-
-
-
-
